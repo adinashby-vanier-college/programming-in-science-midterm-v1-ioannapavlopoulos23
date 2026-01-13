c25064cb9a10fd4ab607758472d71dfb4df7e9eb
ioannapavlopoulos23 <ioannapavlopoulos2313@gmail.com>
2026-01-13 11:04:27 -0500

    Implement area calculation and triangle/pyramid functions
    Implemented area calculation for a circle, hollow right triangle, and inverted pyramid. Updated function definitions and added input validation.

--- Midterm.py
+++ Midterm.py
@@ -2,34 +2,66 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    area = math.pi * radius ** 2
+    return round(area , 2)
+
+
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    if n < 4 :
+        return "The triangle height should be at least 4."
+    result = ""
+    
+    for i in range(n):
+        for j in range(i + 1):
+           if j == 0 or j == i or i == n - 1:
+                result += "*"
+           else:
+                result += " "
+        if i!= n - 1:
+            result += "\n"
+    return result
+
+
+
+
+    
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
-    return ""
+    if n < 3 :
+        return "The pyramid height should be at least 3."
+    result = ""
+    for i in range(n):
+        spaces = i
+        stars = 2 * n - 2 * i - 1
+        result += " " * spaces
+        for j in range(stars):
+            result += "*"
+        if i != n-1 :
+            result += "\n"
+    return result
 
-# ----------------------------------------------------------------
-print(area_of_circle(5))
-print()
 
-print(hollow_right_triangle(3))
-print()
+# # ----------------------------------------------------------------
+# print(area_of_circle(5))
+# print()
 
-print(hollow_right_triangle(4))
-print()
+# print(hollow_right_triangle(3))
+# print()
 
-print(hollow_right_triangle(5))
-print()
+# print(hollow_right_triangle(4))
+# print()
 
-print(inverted_pyramid(3))
-print()
+# print(hollow_right_triangle(5))
+# print()
 
-print(inverted_pyramid(4))
-print()
+# print(inverted_pyramid(3))
+# print()
 
-print(inverted_pyramid(5))
-print()
+# print(inverted_pyramid(4))
+# print()
+
+# print(inverted_pyramid(5))
+# print()
